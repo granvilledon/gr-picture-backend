605da206ab114fa2be2be60fbb675a65079ed644
Granville Don <13393651+granvilledon@user.noreply.gitee.com>
2025-03-31 22:00:40 +0800

    Initial commit

---
+++ src/main/java/com/granvilledon/grpicturebackend/GrPictureApplication.java
@@ -0,0 +1,13 @@
+package com.granvilledon.grpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrPictureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/granvilledon/grpicturebackend/GrPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granvilledon.grpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
